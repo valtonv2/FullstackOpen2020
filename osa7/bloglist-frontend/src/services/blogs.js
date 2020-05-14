510b42e0f02c04f03cfb3e013c670e7af866f6ee
Valtteri Valtonen <valtonenvaltteri@gmail.com>
2020-05-14 16:10:16 +0300

    Remove token prints

--- osa7/bloglist-frontend/src/services/blogs.js
+++ osa7/bloglist-frontend/src/services/blogs.js
@@ -4,19 +4,16 @@ const baseUrl = 'http://localhost:3003/api/blogs/'
 let token = null
 
 const setToken = (newToken) => {
-  console.log('Set token: ', newToken)
   token = `bearer ${newToken}`
 }
 
 const getAll = async () => {
-  console.log('Token', token)
   const response = await axios.get(baseUrl)
   return response.data
 }
 
 const addOne = async (blog) => {
 
-  console.log('token ', token)
   const config = { headers: { Authorization: token } }
 
   const response = await axios.post(baseUrl, blog, config)
@@ -26,7 +23,6 @@ const addOne = async (blog) => {
 
 const updateOne = async (blog) => {
 
-  console.log('token ', token)
   const config = { headers: { Authorization: token } }
 
   const response = await axios.put(baseUrl+blog.id, blog, config)
@@ -36,7 +32,6 @@ const updateOne = async (blog) => {
 
 const deleteOne = async (blog) => {
 
-  console.log('token ', token)
   const config = { headers: { Authorization: token } }
 
   const response = await axios.delete((baseUrl+blog.id), config)
